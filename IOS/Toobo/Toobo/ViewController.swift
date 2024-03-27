65ec0140869f004a71296fc6d07c4eb981dc89cc
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-03-27 17:57:24 +0100

    [ADD] Welcome Page

---
+++ IOS/Toobo/Toobo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toobo
+//
+//  Created by Rémi Desbordes on 27/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
