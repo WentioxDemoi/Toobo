edb6edcefe105ad5a9cafe68ba38da202ea3bb6f
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-03-28 18:59:09 +0100

    [MODIF] Change from XCode Sotryboard to Xcode swift

---
+++ IOS/Toobo/Toobo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Toobo
+//
+//  Created by Rémi Desbordes on 28/03/2024.
+//
+
+import SwiftUI
+//import Front.AuthPage.AuthPage
+
+
+
+struct ContentView: View {
+    var body: some View {
+        WelcomePage()
+    }
+}
+
+#Preview {
+    ContentView()
+}
